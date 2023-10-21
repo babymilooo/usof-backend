d7f318cfbca47c3b9ff2eb35b950b990791984c3
babymilooo <Kyrylo.Yehorov@cit.khpi.edu.ua>
2023-10-21 14:25:40 +0300

    rebuild

--- routes/socketRouter.js
+++
@@ -1,9 +0,0 @@
-import controller from "../controllers/socketController.js";
-
-export default function socketRouter(io, socket) {
-    let userData = {};
-
-    socket.on("getUserData", async (data) => {
-        userData = await controller.getUserData(io, socket, data);
-    });
-}
